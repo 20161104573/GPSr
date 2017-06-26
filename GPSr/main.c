5f3fb15a2c91db2f23fdbbed559c461222808e34
20161104573 <a20161104573@admindeiMac-34.local>
2017-06-26 10:42:01 +0800

    Read GPS
    读取GPS文件里的纬度

--- GPSr/main.c
+++ GPSr/main.c
@@ -10,6 +10,18 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    FILE *fr;
+    char str1[70];
+    char str2[70];
+    char lat[9];
+    int i;
+    fr=fopen("//Users//a20161104573//Desktop//GPS170510.log","r+");
+    fscanf(fr,"%s%s",str1,str2);
+    printf("结果:%s\n%s\n",str1,str2);
+    for(i=0;i<8;i++)
+        lat[i]=str1[i+16];
+    lat[8]='\n';
+    printf("纬度:%s\n",lat);
+    fclose(fr);
     return 0;
 }
